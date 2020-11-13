32de223b209d7bf7365cb05bd73158046c782668
kaho1910 <51158745+kaho1910@users.noreply.github.com>
2020-11-13 10:16:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
         <a href="ITF webtech/work1/index1.html">web01</a><br>
         <a href="ITF webtech/work2 and 3/index2.html">web02</a><br>
         <a href="ITF webtech/work2 and 3/index3.html">web03</a><br>
-        <a href="ITF webtech/work4/show.html">web04</a><br>
+        <a href="ITF webtech/work4/show.php">web04</a><br>
     </body>
 </html>
