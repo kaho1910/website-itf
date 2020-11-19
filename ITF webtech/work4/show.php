e4220481fd5e66eacaa2fda4b703b780d5ff60d7
BuildTools <unconfigured@null.spigotmc.org>
2020-11-19 20:55:38 +0700

    Update show.php

--- ITF webtech/work4/show.php
+++ ITF webtech/work4/show.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-  <title>Bootstrap Example</title>
+  <title>ITF-lab</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -57,6 +57,6 @@
   <?php
   mysqli_close($conn);
   ?>
-  <a href="form.html" class="button button1" style="background-color: transparent; color:black">Add</a>
+  <button href="form.html" class="button button1">Add</button>
   </body>
 </html>
