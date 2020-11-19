6c88f28992097f5d549a2bf8446804683313c1ce
BuildTools <unconfigured@null.spigotmc.org>
2020-11-19 20:39:32 +0700

    update

--- ITF webtech/work4/show.php
+++ ITF webtech/work4/show.php
@@ -1,13 +1,12 @@
 <html>
-  <head>
-    <title>ITF Lab</title>
-    <title>Bootstrap Example</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  </head>
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
   <body>
     <?php
     $conn = mysqli_init();
@@ -18,7 +17,7 @@
     }
     $res = mysqli_query($conn, 'SELECT * FROM guestbook');
     ?>
-    <table class="table table-dark">
+    <table class="table table-striped">
       <thead>
         <tr>
           <th><div>Name</div></th>
@@ -30,11 +29,13 @@
       while($Result = mysqli_fetch_array($res))
       {
       ?>
+      <tbody>
         <tr class="table-primary">
           <td><?php echo $Result['Name'];?></div></td>
           <td><?php echo $Result['Comment'];?></td>
           <td><?php echo $Result['Link'];?></td>
         </tr>
+      </tbody>
       <?php
       }
       ?>
