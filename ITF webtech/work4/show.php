ae076e55483567b6ff05964567ef970e13955917
BuildTools <unconfigured@null.spigotmc.org>
2020-11-19 20:36:54 +0700

    Update show.php

--- ITF webtech/work4/show.php
+++ ITF webtech/work4/show.php
@@ -1,46 +1,46 @@
 <html>
-<head>
-  <title>ITF Lab</title>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'itfwebtechsql.mysql.database.azure.com', 'kaho1910@itfwebtechsql', 'Tawan1910', 'work', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
-?>
-<table class="table table-dark">
-  <thead class="thead-dark">
-    <tr>
-      <th width="100"> <div align="center">Name</div></th>
-      <th width="350"> <div align="center">Comment </div></th>
-      <th width="150"> <div align="center">Link </div></th>
-    </tr>
-  </thead>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr class="table-primary">
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-  </tr>
-<?php
-}
-?>
-</table>
-<?php
-mysqli_close($conn);
-?>
-</body>
+  <head>
+    <title>ITF Lab</title>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <?php
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, 'itfwebtechsql.mysql.database.azure.com', 'kaho1910@itfwebtechsql', 'Tawan1910', 'work', 3306);
+    if (mysqli_connect_errno($conn))
+    {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }
+    $res = mysqli_query($conn, 'SELECT * FROM guestbook');
+    ?>
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <th><div>Name</div></th>
+          <th><div>Comment </div></th>
+          <th><div>Link </div></th>
+        </tr>
+      </thead>
+      <?php
+      while($Result = mysqli_fetch_array($res))
+      {
+      ?>
+        <tr class="table-primary">
+          <td><?php echo $Result['Name'];?></div></td>
+          <td><?php echo $Result['Comment'];?></td>
+          <td><?php echo $Result['Link'];?></td>
+        </tr>
+      <?php
+      }
+      ?>
+    </table>
+  <?php
+  mysqli_close($conn);
+  ?>
+  </body>
 </html>
