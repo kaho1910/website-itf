335237fca849bc2c923c8844be023433712af495
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 02:57:53 +0700

    Update formUpdate.php

--- ITF webtech/work4/formUpdate.php
+++ ITF webtech/work4/formUpdate.php
@@ -36,7 +36,7 @@
     while($Result = mysqli_fetch_array($res))
     {
   ?>
-  <form action="update.php" method="update" id="CommentForm">
+  <form action="update.php" method="post" id="CommentForm">
     <div class="form-group">
       <label for="idName">Name</label><br>
       <input type="text" class="form-control" name="name" value="<?php echo $Result['name'];?>" id="idName">
