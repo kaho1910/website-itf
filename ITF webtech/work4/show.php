0499c1d51034137bc7d8f43452d1324bb719f4c6
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 00:38:00 +0700

    Update show.php

--- ITF webtech/work4/show.php
+++ ITF webtech/work4/show.php
@@ -35,7 +35,9 @@
         <td width="100"><?php echo $Result['Name'];?></div></td>
         <td width="300"><?php echo $Result['Comment'];?></td>
         <td width="100"><?php echo $Result['Link'];?></td>
-        <td width="100"><div style="text-align: center"><a href="update.php" class="btn btn-primary">Edit</a></div></td>
+        <td width="50"><div style="text-align: center"><a href="test.php" class="btn btn-primary">Edit</a></div></td>
+        <td width="50"><div style="text-align: center"><a href="test.php" class="btn btn-primary">Delete</a></div></td>
+
       </tr>
     </tbody>
     <?php
