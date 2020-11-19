8c6924c8762fbbc23f30d6949048a00c42497758
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 03:29:01 +0700

    Update formUpdate.php

--- ITF webtech/work4/formUpdate.php
+++ ITF webtech/work4/formUpdate.php
@@ -30,20 +30,22 @@
         die('Failed to connect to MySQL: '.mysqli_connect_error());
     }
     $id = $_GET['id'];
-    $res = mysqli_query($conn, "SELECT * FROM guestbook WHERE id=$id");
   ?>
   <form action="update.php" method="post" id="UpdateForm">
+    <?php
+      $res = mysqli_query($conn, "SELECT * FROM guestbook WHERE id=$id");
+    ?>
     <div class="form-group">
       <label for="idName">Name</label><br>
-      <input type="text" class="form-control" name="name" value="<?php echo $res['name'];?>" id="idName">
+      <input type="text" class="form-control" name="name" value=<?php echo $res['name'];?> id="idName">
     </div>
     <div class="form-group">
       <label for="idComment">Comment</label><br>
-      <input type="text" class="form-control" name="comment" value="<?php echo $res['comment'];?>" id="idComment">
+      <input type="text" class="form-control" name="comment" value=<?php echo $res['comment'];?> id="idComment">
     </div>
     <div class="form-group">
       <label for="idLink">Link</label><br>
-      <input type="text" class="form-control" name="link" value="<?php echo $res['link'];?>" id="idLink">
+      <input type="text" class="form-control" name="link" value=<?php echo $res['link'];?> id="idLink">
     </div>
     <button type="submit" class="button button1">Submit</button>
   </form>
