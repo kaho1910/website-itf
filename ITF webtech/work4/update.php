fa783be197ba9f4048acfe0185dba1c9d67b8b61
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 04:36:27 +0700

    Update update.php

--- ITF webtech/work4/update.php
+++ ITF webtech/work4/update.php
@@ -7,12 +7,12 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$id = $_REQUEST['id'];
+$id = $_GET['id'];
 $name = $_POST['name'];
 $comment = $_POST['comment'];
 $link = $_POST['link'];
 
-$sql = "UPDATE guestbook SET Name='$name', Comment='$comment', Link='$link' WHERE ID='$id'";
+$sql = "UPDATE guestbook SET Name='$name', Comment='$comment', Link='$link' WHERE id='$id'";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record updated successfully <br><a href='show.php' class='btn btn-primary'>back to Main</a>";
