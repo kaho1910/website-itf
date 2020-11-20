1cfd05baa8d72063733558c05143306acc9cbf07
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 11:44:13 +0700

    update7
    try

--- ITF webtech/work4/formAdd.php
+++ ITF webtech/work4/formAdd.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html>
 <head>
   <title>Comment Form</title>
--- ITF webtech/work4/formUpdate.php
+++ ITF webtech/work4/formUpdate.php
@@ -1,7 +1,6 @@
-<!DOCTYPE html>
 <html>
 <head>
-  <title>Update Form</title>
+  <title>Edit Comment Form</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
