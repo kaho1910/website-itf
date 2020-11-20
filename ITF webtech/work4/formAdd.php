07ce5dbecb50f436a51b40de3b2124e16d23d191
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 11:34:21 +0700

    Update formAdd.php

--- ITF webtech/work4/formAdd.php
+++ ITF webtech/work4/formAdd.php
@@ -7,8 +7,8 @@
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
   <style>
-    .btn {
-      border: 1px green;
+    .button {
+      border: 1px solid black;
       background-color: white;
       color: white;
       padding: 10px 18px;
