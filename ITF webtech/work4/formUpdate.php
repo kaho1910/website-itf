1ba67ab4711bce235fce6cad8dfc88964e19da1f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 11:39:18 +0700

    update
    change template formAdd and formUpdate

--- ITF webtech/work4/formUpdate.php
+++ ITF webtech/work4/formUpdate.php
@@ -2,10 +2,11 @@
 <html>
 <head>
   <title>Update Form</title>
-  </style>
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
   <?php
