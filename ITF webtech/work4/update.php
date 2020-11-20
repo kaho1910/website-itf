b462a17fcbcc34f21d20131272b012fbecd890b8
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 09:54:03 +0700

    Update update.php

--- ITF webtech/work4/update.php
+++ ITF webtech/work4/update.php
@@ -14,7 +14,7 @@ $link = $_REQUEST['link'];
 
 $sql = "UPDATE guestbook SET Name='$name', Comment='$comment', Link='$link' WHERE ID=$id";
 
-if (mysqli_query($sql, $conn)) {
+if (mysqli_query($conn, $sql)) {
     echo "New record updated successfully <br><a href='show.php' class='btn btn-primary'>back to Main</a>";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
